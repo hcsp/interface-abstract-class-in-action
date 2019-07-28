3b4200179fd17f94c91b527035969ad06612d96e
shiliyu <liyu.italy@hotmail.com>
2019-07-28 19:36:58 +0800

    fix onlyAFewInstanceof test error

--- src/main/java/com/github/hcsp/polymorphism/World.java
+++ src/main/java/com/github/hcsp/polymorphism/World.java
@@ -18,19 +18,6 @@ public class World {
         }
     }
 
-    //    public static void 会飞的东西飞() {
-//        for (Object obj : objects) {
-//            if (obj instanceof 麻雀) {
-//                ((麻雀) obj).飞();
-//            } else if (obj instanceof 喜鹊) {
-//                ((喜鹊) obj).飞();
-//            } else if (obj instanceof 蝴蝶) {
-//                ((蝴蝶) obj).飞();
-//            } else if (obj instanceof 飞机) {
-//                ((飞机) obj).飞();
-//            }
-//        }
-//    }
     // 在建造成类型体系后，请尝试化简这个啰嗦的方法，体会多态带来的好处
     public static void 会叫的东西叫() {
         for (Object obj : objects) {
@@ -39,21 +26,7 @@ public class World {
             }
         }
     }
-//    public static void 会叫的东西叫() {
-//        for (Object obj : objects) {
-//            if (obj instanceof 麻雀) {
-//                ((麻雀) obj).叫();
-//            } else if (obj instanceof 喜鹊) {
-//                ((喜鹊) obj).叫();
-//            } else if (obj instanceof 救护车) {
-//                ((救护车) obj).叫();
-//            } else if (obj instanceof 猫) {
-//                ((猫) obj).叫();
-//            } else if (obj instanceof 狗) {
-//                ((狗) obj).叫();
-//            }
-//        }
-//    }
+
     // 在建造成类型体系后，请尝试化简这个啰嗦的方法，体会多态带来的好处
     public static void 动物都能新陈代谢() {
         for (Object obj : objects) {
@@ -62,22 +35,6 @@ public class World {
             }
         }
     }
-
-//    public static void 动物都能新陈代谢() {
-//        for (Object obj : objects) {
-//            if (obj instanceof 麻雀) {
-//                ((麻雀) obj).新陈代谢();
-//            } else if (obj instanceof 喜鹊) {
-//                ((喜鹊) obj).新陈代谢();
-//            } else if (obj instanceof 蝴蝶) {
-//                ((蝴蝶) obj).新陈代谢();
-//            } else if (obj instanceof 猫) {
-//                ((猫) obj).新陈代谢();
-//            } else if (obj instanceof 狗) {
-//                ((狗) obj).新陈代谢();
-//            }
-//        }
-//    }
 
     static class 麻雀 extends 鸟 {}
 
