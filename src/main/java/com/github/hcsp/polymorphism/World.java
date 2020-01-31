2a68d3c06373e480b67f5a3ce96239b4e6536e59
nicknessii <545310317@qq.com>
2020-01-31 18:42:03 +0800

    接口与抽象类

--- src/main/java/com/github/hcsp/polymorphism/World.java
+++ src/main/java/com/github/hcsp/polymorphism/World.java
@@ -8,16 +8,14 @@ public class World {
     // 以最大限度的复用、简化代码
     public static List<Object> objects =
             Arrays.asList(new 麻雀(), new 喜鹊(), new 蝴蝶(), new 飞机(), new 救护车(), new 猫(), new 狗());
-
     // 在建造成类型体系后，请尝试化简这个啰嗦的方法，体会多态带来的好处
     public static void 会飞的东西飞() {
         for (Object obj : objects) {
             if (obj instanceof 会飞的东西) {
-                ((会飞的东西 obj).飞();
+                ((会飞的东西) obj).飞();
             }
         }
     }
-
     // 在建造成类型体系后，请尝试化简这个啰嗦的方法，体会多态带来的好处
     public static void 会叫的东西叫() {
         for (Object obj : objects) {
@@ -26,13 +24,67 @@ public class World {
             }
         }
     }
-
     // 在建造成类型体系后，请尝试化简这个啰嗦的方法，体会多态带来的好处
     public static void 动物都能新陈代谢() {
         for (Object obj : objects) {
             if (obj instanceof 活的动物) {
                 ((活的动物) obj).新陈代谢();
             }
+        }
+    }
+    static class 活的动物 implements 动物 {
+        public void 新陈代谢() {
+            System.out.println("新陈代谢");
+        }
+    }
+    static class 鸟 extends 活的动物 implements 会叫的东西,会飞的东西{
+        public void 飞() {
+            System.out.println("鸟儿飞");
+        }
+
+        public void 叫() {
+            System.out.println("叽叽喳喳");
+        }
+    }
+
+    static class 麻雀 extends 鸟 {
+               }
+
+
+
+    static class 喜鹊 extends 鸟 {
+                }
+
+
+    static class 蝴蝶 extends 活的动物 implements 会飞的东西{
+
+        public void 飞() {
+            System.out.println("蝴蝶飞");
+        }
+    }
+
+        static class 飞机 implements 会飞的东西 {
+
+        public void 飞() {
+            System.out.println("飞机飞");
+        }
+    }
+
+        static class 救护车 implements 会叫的东西 {
+        public void 叫() {
+            System.out.println("哇呜哇呜");
+        }
+    }
+
+        static class 猫 extends 活的动物 implements 会叫的东西{
+        public void 叫() {
+            System.out.println("喵喵喵");
+        }
+    }
+
+     static class 狗 extends 活的动物 implements 会叫的东西{
+        public void 叫() {
+            System.out.println("汪汪汪");
         }
     }
 
@@ -47,71 +99,4 @@ public class World {
     interface 会叫的东西 {
         void 叫();
     }
-
-    static class 活的动物 implements 动物 {
-
-        public void 新陈代谢() {
-
-            System.out.println("新陈代谢");
-        }
-    }
-
-    static class 鸟 extends 活的动物 implements 会飞的东西, 会叫的东西 {
-
-        public void 飞() {
-            System.out.println("鸟儿飞");
-        }
-
-
-        public void 叫() {
-            System.out.println("叽叽喳喳");
-        }
-    }
-
-       static class 麻雀 extends 鸟  {
-
-    }
-
-    static class 喜鹊 extends 鸟  {
-
-    }
-
-    static class 蝴蝶 extends 活的动物 implements 会飞的东西 {
-
-        public void 飞() {
-            System.out.println("蝴蝶飞");
-        }
-    }
-
-    static class 飞机 implements 会飞的东西 {
-
-        public void 飞() {
-
-            System.out.println("飞机飞");
-        }
-    }
-
-    static class 救护车 implements 会叫的东西 {
-
-        public void 叫() {
-
-            System.out.println("哇呜哇呜");
-        }
-    }
-
-    static class 猫 extends 活的动物 implements 会叫的东西 {
-
-        public void 叫() {
-            System.out.println("喵喵喵");
-        }
-    }
-
-    static class 狗 extends 活的动物 implements 会叫的东西 {
-
-        public void 叫() {
-            System.out.println("汪汪汪");
-        }
-    }
-
-
 }
