42683613befd8a5b176d01c5ec286793c241c865
theyhq <1076883793@qq.com>
2020-03-07 00:10:22 +0800

    简化类们3

--- src/main/java/com/github/hcsp/polymorphism/World.java
+++ src/main/java/com/github/hcsp/polymorphism/World.java
@@ -33,28 +33,10 @@ public class World {
     }
     }
 
-    static class 麻雀 extends 动物 implements 会飞的东西,会叫的东西{
-
-        @Override
-        public void 飞() {
-            System.out.println("鸟儿飞");
-        }
-
-        @Override
-        public void 叫() {
-            System.out.println("叽叽喳喳");
-        }
+    static class 麻雀 extends 鸟 implements 会飞的东西,会叫的东西{
     }
 
-    static class 喜鹊 extends 动物 implements 会飞的东西,会叫的东西{
-
-        public void 飞() {
-            System.out.println("鸟儿飞");
-        }
-
-        public void 叫() {
-            System.out.println("叽叽喳喳");
-        }
+    static class 喜鹊 extends 鸟 implements 会飞的东西,会叫的东西{
     }
 
     static class 蝴蝶 extends 动物 implements 会飞的东西{
@@ -76,7 +58,6 @@ public class World {
     }
 
     static class 猫 extends 动物 implements 会叫的东西{
-
         public void 叫() {
             System.out.println("喵喵喵");
         }
@@ -92,6 +73,19 @@ public class World {
     static class 动物 {
         void 新陈代谢(){
             System.out.println("新陈代谢");
+        }
+    }
+
+    static class 鸟 extends 动物 implements 会飞的东西,会叫的东西 {
+
+        @Override
+        public void 飞() {
+            System.out.println("鸟儿飞");
+        }
+
+        @Override
+        public void 叫() {
+            System.out.println("叽叽喳喳");
         }
     }
 
