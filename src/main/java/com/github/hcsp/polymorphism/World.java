d6eafd1ed1ceaa80c7521ea2c89ac93e072cca0d
DobbyKim <1175088275@qq.com>
2020-03-08 19:10:54 +0800

    add code

--- src/main/java/com/github/hcsp/polymorphism/World.java
+++ src/main/java/com/github/hcsp/polymorphism/World.java
@@ -34,37 +34,14 @@ public class World {
     }
 
     static class 麻雀 extends 鸟{
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
 
-        public void 飞() {
-            System.out.println("鸟儿飞");
-        }
-
-        public void 叫() {
-            System.out.println("叽叽喳喳");
-        }
     }
 
     static class 喜鹊 extends 鸟{
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
 
-        public void 飞() {
-            System.out.println("鸟儿飞");
-        }
-
-        public void 叫() {
-            System.out.println("叽叽喳喳");
-        }
     }
 
     static class 蝴蝶 extends 活的动物 implements 会飞的东西{
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
 
         public void 飞() {
             System.out.println("蝴蝶飞");
@@ -84,20 +61,12 @@ public class World {
     }
 
     static class 猫 extends 活的动物 implements 会叫的东西{
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
-
         public void 叫() {
             System.out.println("喵喵喵");
         }
     }
 
     static class 狗  extends 活的动物 implements 会叫的东西{
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
-
         public void 叫() {
             System.out.println("汪汪汪");
         }
