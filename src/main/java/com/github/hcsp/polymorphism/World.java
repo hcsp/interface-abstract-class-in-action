57f8947b6d21ce6a81dc42a9cb61548c0c568dca
alex <douknowtheshy@gmail.com>
2020-09-11 20:04:04 -0700

    aa

--- src/main/java/com/github/hcsp/polymorphism/World.java
+++ src/main/java/com/github/hcsp/polymorphism/World.java
@@ -54,72 +54,52 @@ public class World {
             }
         }
     }
-
-    static class 麻雀 {
+    static class 活的动物 implements 动物{
         public void 新陈代谢() {
             System.out.println("新陈代谢");
         }
-
+    }
+    static class 鸟 extends 活的动物 implements 会飞的东西,会叫的东西{
         public void 飞() {
             System.out.println("鸟儿飞");
         }
-
+        @Override
         public void 叫() {
             System.out.println("叽叽喳喳");
         }
     }
 
-    static class 喜鹊 {
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
-
-        public void 飞() {
-            System.out.println("鸟儿飞");
-        }
-
-        public void 叫() {
-            System.out.println("叽叽喳喳");
-        }
+    static class 麻雀 extends 鸟{
     }
 
-    static class 蝴蝶 {
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
+    static class 喜鹊 extends 鸟{
+    }
 
+    static class 蝴蝶 extends 活的动物 implements 会飞的东西{
         public void 飞() {
             System.out.println("蝴蝶飞");
         }
     }
 
-    static class 飞机 {
+    static class 飞机 implements 会飞的东西{
         public void 飞() {
             System.out.println("飞机飞");
         }
     }
 
-    static class 救护车 {
+    static class 救护车 implements 会叫的东西{
         public void 叫() {
             System.out.println("哇呜哇呜");
         }
     }
 
-    static class 猫 {
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
-
+    static class 猫 extends 活的动物 implements 会叫的东西{
         public void 叫() {
             System.out.println("喵喵喵");
         }
     }
 
-    static class 狗 {
-        public void 新陈代谢() {
-            System.out.println("新陈代谢");
-        }
-
+    static class 狗 extends 活的动物 implements 会叫的东西{
         public void 叫() {
             System.out.println("汪汪汪");
         }
