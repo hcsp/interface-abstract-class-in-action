be5864d4496f22cfe0671e91aa03b9a6cfb073c8
soohea <sooheaa@gmai.com>
2021-10-15 14:05:39 +0800

    interface-abstract-class-in-action1.0

--- src/main/java/com/github/hcsp/polymorphism/World.java
+++ src/main/java/com/github/hcsp/polymorphism/World.java
@@ -9,20 +9,7 @@ public class World {
     public static List<Object> objects =
             Arrays.asList(new 麻雀(), new 喜鹊(), new 蝴蝶(), new 飞机(), new 救护车(), new 猫(), new 狗());
 
-    // 在建造成类型体系后，请尝试化简这个啰嗦的方法，体会多态带来的好处
-//    public static void 会飞的东西飞() {
-//        for (Object obj : objects) {
-//            if (obj instanceof 麻雀) {
-//                ((麻雀) obj).飞();
-//            } else if (obj instanceof 喜鹊) {
-//                ((喜鹊) obj).飞();
-//            } else if (obj instanceof 蝴蝶) {
-//                ((蝴蝶) obj).飞();
-//            } else if (obj instanceof 飞机) {
-//                ((飞机) obj).飞();
-//            }
-//        }
-//    }
+
     public static void 会飞的东西飞() {
         for (Object obj : objects) {
             if (obj instanceof 会飞的东西) {
@@ -32,22 +19,7 @@ public class World {
 
     }
 
-    // 在建造成类型体系后，请尝试化简这个啰嗦的方法，体会多态带来的好处
-//    public static void 会叫的东西叫() {
-//        for (Object obj : objects) {
-//            if (obj instanceof 麻雀) {
-//                ((麻雀) obj).叫();
-//            } else if (obj instanceof 喜鹊) {
-//                ((喜鹊) obj).叫();
-//            } else if (obj instanceof 救护车) {
-//                ((救护车) obj).叫();
-//            } else if (obj instanceof 猫) {
-//                ((猫) obj).叫();
-//            } else if (obj instanceof 狗) {
-//                ((狗) obj).叫();
-//            }
-//        }
-//    }
+
     public static void 会叫的东西叫() {
         for (Object obj : objects) {
             if (obj instanceof 会叫的东西) {
@@ -56,22 +28,7 @@ public class World {
         }
     }
 
-    // 在建造成类型体系后，请尝试化简这个啰嗦的方法，体会多态带来的好处
-//    public static void 动物都能新陈代谢() {
-//        for (Object obj : objects) {
-//            if (obj instanceof 麻雀) {
-//                ((麻雀) obj).新陈代谢();
-//            } else if (obj instanceof 喜鹊) {
-//                ((喜鹊) obj).新陈代谢();
-//            } else if (obj instanceof 蝴蝶) {
-//                ((蝴蝶) obj).新陈代谢();
-//            } else if (obj instanceof 猫) {
-//                ((猫) obj).新陈代谢();
-//            } else if (obj instanceof 狗) {
-//                ((狗) obj).新陈代谢();
-//            }
-//        }
-//    }
+
     public static void 动物都能新城代谢() {
         for (Object obj : objects) {
             if (obj instanceof 动物) {
@@ -91,19 +48,9 @@ public class World {
         public void 叫() {
             System.out.println("叽叽喳喳");
         }
-
-//        public void 新陈代谢() {
-//            System.out.println("新陈代谢");
-//        }
-//
-//        public void 飞() {
-//            System.out.println("鸟儿飞");
-//        }
-//
-//        public void 叫() {
-//            System.out.println("叽叽喳喳");
-//        }
     }
+
+
 
     static class 喜鹊 implements 动物, 会飞的东西, 会叫的东西 {
         @Override
@@ -115,34 +62,18 @@ public class World {
         public void 叫() {
             System.out.println("叽叽喳喳");
         }
-
-//        public void 新陈代谢() {
-//            System.out.println("新陈代谢");
-//        }
-//
-//        public void 飞() {
-//            System.out.println("鸟儿飞");
-//        }
-//
-//        public void 叫() {
-//            System.out.println("叽叽喳喳");
-//        }
     }
+
+
 
     static class 蝴蝶 implements 动物, 会飞的东西 {
         @Override
         public void 飞() {
             System.out.println("蝴蝶飞");
         }
-
-//        public void 新陈代谢() {
-//            System.out.println("新陈代谢");
-//        }
-//
-//        public void 飞() {
-//            System.out.println("蝴蝶飞");
-//        }
     }
+
+
 
     static class 飞机 implements 会飞的东西 {
         @Override
